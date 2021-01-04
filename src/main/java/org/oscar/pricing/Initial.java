e78267243c5c18a877871c3ea1029f21ea470269
oskiegarcia <ogarc74@gmail.com>
2021-01-04 22:30:58 +0800

    added Initial class for better readability of PriceGenerator

---
+++ src/main/java/org/oscar/pricing/Initial.java
@@ -0,0 +1,10 @@
+package org.oscar.pricing;
+
+import java.math.BigDecimal;
+
+public class Initial {
+
+    public static BigDecimal valueOf(double value){
+        return BigDecimal.valueOf(value);
+    }
+}
--- src/main/java/org/oscar/pricing/provider/PriceGenerator.java
+++ src/main/java/org/oscar/pricing/provider/PriceGenerator.java
@@ -1,6 +1,7 @@
 package org.oscar.pricing.provider;
 
 import org.oscar.pricing.CryptoPair;
+import org.oscar.pricing.Initial;
 import org.oscar.pricing.Volatility;
 import org.oscar.pricing.engine.CryptoPriceEngine;
 import org.oscar.pricing.engine.PriceEngine;
@@ -18,35 +19,35 @@ public class PriceGenerator implements PriceProvider {
         engine = new HashMap<>();
 
         // BTC/USD
-        PriceEngine btcUsd = CryptoPriceEngine.create(BigDecimal.valueOf(30000.0d), Volatility.range(1, 15));
+        PriceEngine btcUsd = CryptoPriceEngine.create(Initial.valueOf(30000.0d), Volatility.range(1, 15));
         engine.put(CryptoPair.of("BTC", "USD"), btcUsd);
 
         // ETH/USD
-        PriceEngine ethUsd = CryptoPriceEngine.create(BigDecimal.valueOf(900.0d), Volatility.range(1, 10));
+        PriceEngine ethUsd = CryptoPriceEngine.create(Initial.valueOf(900.0d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("ETH", "USD"), ethUsd);
 
         // LTC/USD
-        PriceEngine ltcUsd = CryptoPriceEngine.create(BigDecimal.valueOf(155.0d), Volatility.range(1, 10));
+        PriceEngine ltcUsd = CryptoPriceEngine.create(Initial.valueOf(155.0d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("LTC", "USD"), ltcUsd);
 
         // DOT/USD
-        PriceEngine dotUsd = CryptoPriceEngine.create(BigDecimal.valueOf(9.0d), Volatility.range(1, 10));
+        PriceEngine dotUsd = CryptoPriceEngine.create(Initial.valueOf(9.0d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("DOT", "USD"), dotUsd);
 
         // UNI/USD
-        PriceEngine uniUsd = CryptoPriceEngine.create(BigDecimal.valueOf(5.0d), Volatility.range(1, 10));
+        PriceEngine uniUsd = CryptoPriceEngine.create(Initial.valueOf(5.0d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("UNI", "USD"), uniUsd);
 
         // XRP/USD
-        PriceEngine xrpUsd = CryptoPriceEngine.create(BigDecimal.valueOf(0.2280d), Volatility.range(1, 20));
+        PriceEngine xrpUsd = CryptoPriceEngine.create(Initial.valueOf(0.2280d), Volatility.range(1, 20));
         engine.put(CryptoPair.of("XRP", "USD"), xrpUsd);
 
         // BAT/USD
-        PriceEngine batUsd = CryptoPriceEngine.create(BigDecimal.valueOf(0.2089d), Volatility.range(1, 10));
+        PriceEngine batUsd = CryptoPriceEngine.create(Initial.valueOf(0.2089d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("XRP", "USD"), batUsd);
 
         // ZIL/USD
-        PriceEngine zilUsd = CryptoPriceEngine.create(BigDecimal.valueOf(0.0690d), Volatility.range(1, 10));
+        PriceEngine zilUsd = CryptoPriceEngine.create(Initial.valueOf(0.0690d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("XRP", "USD"), zilUsd);
 
     }
