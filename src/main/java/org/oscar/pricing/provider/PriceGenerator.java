dd0451b642c218e504f9993a2749356781fdf0af
oskiegarcia <ogarc74@gmail.com>
2021-01-05 10:46:21 +0800

    added stellar

--- src/main/java/org/oscar/pricing/provider/PriceGenerator.java
+++ src/main/java/org/oscar/pricing/provider/PriceGenerator.java
@@ -13,7 +13,7 @@ import java.util.Map;
 public class PriceGenerator implements PriceProvider {
 
 
-    private Map<CryptoPair, PriceEngine> engine;
+    private final Map<CryptoPair, PriceEngine> engine;
 
     private PriceGenerator() {
         engine = new HashMap<>();
@@ -25,37 +25,42 @@ public class PriceGenerator implements PriceProvider {
     }
 
     private void addPriceEngines() {
-        // BTC/USD
+        // BTC/USD - Bitcoin
         PriceEngine btcUsd = CryptoPriceEngine.create(Initial.valueOf(30000.0d), Volatility.range(1, 15));
         engine.put(CryptoPair.of("BTC", "USD"), btcUsd);
 
-        // ETH/USD
+        // ETH/USD - Ethereum
         PriceEngine ethUsd = CryptoPriceEngine.create(Initial.valueOf(900.0d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("ETH", "USD"), ethUsd);
 
-        // LTC/USD
+        // LTC/USD - Litecoin
         PriceEngine ltcUsd = CryptoPriceEngine.create(Initial.valueOf(155.0d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("LTC", "USD"), ltcUsd);
 
-        // DOT/USD
+        // DOT/USD - Polkadot
         PriceEngine dotUsd = CryptoPriceEngine.create(Initial.valueOf(9.0d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("DOT", "USD"), dotUsd);
 
-        // UNI/USD
+        // UNI/USD - Uniswap
         PriceEngine uniUsd = CryptoPriceEngine.create(Initial.valueOf(5.0d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("UNI", "USD"), uniUsd);
 
-        // XRP/USD
+        // XRP/USD - Ripple
         PriceEngine xrpUsd = CryptoPriceEngine.create(Initial.valueOf(0.2280d), Volatility.range(1, 20));
         engine.put(CryptoPair.of("XRP", "USD"), xrpUsd);
 
-        // BAT/USD
+        // BAT/USD - Basic Attention Token
         PriceEngine batUsd = CryptoPriceEngine.create(Initial.valueOf(0.2089d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("XRP", "USD"), batUsd);
 
-        // ZIL/USD
+        // ZIL/USD - Ziliqa
         PriceEngine zilUsd = CryptoPriceEngine.create(Initial.valueOf(0.0690d), Volatility.range(1, 10));
         engine.put(CryptoPair.of("XRP", "USD"), zilUsd);
+
+        // XLM/USD - Stellar
+        PriceEngine xlmUsd = CryptoPriceEngine.create(Initial.valueOf(0.0263d), Volatility.range(1, 8));
+        engine.put(CryptoPair.of("XLM", "USD"), xlmUsd);
+
     }
 
 
